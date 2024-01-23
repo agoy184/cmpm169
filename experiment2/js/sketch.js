91b9b0875b754ce8df0dc910c3885b3ebfea016b
Abel Goy <agoy@ucsc.edu>
2024-01-22 18:41:04 -0800

    Update sketch.js

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -29,7 +29,7 @@ class MyClass {
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
@@ -41,25 +41,26 @@ function setup() {
 
     var centerHorz = windowWidth / 2;
     var centerVert = windowHeight / 2;
+    timer = 0;
+    stroke(5);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
+    if (millis() >= 900+timer) {
+        background(mouseX,mouseY,mouseX);
+        timer = millis();
+        fill(mouseX,mouseY,mouseX);
+        box(timer%900, timer%900, 25)
+        print(timer)
+      }  
+      box(mouseX, mouseY, 25);
+      box(mouseX*1.5, mouseY/2, 25);
+      box(mouseY*1.7, mouseY/3, 25);
+      box(25, 25, mouseX*5)
+      circle(height/2, 0, mouseX);
+      circle(-height/2, 0, mouseX);
+    }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
